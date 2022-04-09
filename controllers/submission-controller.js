6ac2c939acd30cb3e6a51bb3eb4f631687aa42a1
pranav1296 <pranavaneja600@gmail.com>
2022-04-09 15:46:34 +0800

    Questions api update

--- controllers/submission-controller.js
+++ controllers/submission-controller.js
@@ -230,7 +230,7 @@ var cleanSQL = async function (DBServerConfig){
 
 }
 
-var getCorrectAnswer = async function (query){
+var getCorrectAnswer = async function (query, DBServerConfig){
     return new Promise((resolve, reject) => {
         executeQuery(query, DBServerConfig)
         .then(result => {
